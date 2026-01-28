e7c83b34d5432dc093f6430a18ae88bba8d47b9d
marcel-ceceu <marcel.franciscato94@gmail.com>
2026-01-28 20:15:28 -0300

    Delete sankhya_vinicius directory

--- sankhya_vinicius/QUERY - Script para analisar as alterações nas TOPs.sql
+++
@@ -1,7 +0,0 @@
-select top.codtipoper,
-top.dhalter,
-top.codusu,
-usu.nomeusu
-from tgftop top, tsiusu usu
-where codtipoper = 1234
-and usu.codusu = top.codusu
--- sankhya_vinicius/QUERY- consultar escrita em objetos no banco de dados, ocorrencias.sql
+++
@@ -1,4 +0,0 @@
-SELECT *
-FROM ALL_SOURCE
-WHERE UPPER(TEXT) LIKE '%Falha ao realizar envio da venda%'
-AND TYPE = 'PROCEDURE';
