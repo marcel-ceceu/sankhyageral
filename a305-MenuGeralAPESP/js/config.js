9990cd2318ae95f24855dcf79880fec7355c7e83
marcel-ceceu <marcel.franciscato94@gmail.com>
2026-01-30 15:02:46 -0300

    Dash funcionando ok

--- a305-MenuGeralAPESP/js/config.js
+++ a305-MenuGeralAPESP/js/config.js
@@ -4,4 +4,4 @@
 
 var ID_BOTAO_ROTINA = 124;
 var NOME_PARAMETRO = 'CPF_CONSULTA';
-var RESOURCE_ID_DASH_ALVO = 'br.com.sankhya.menu.adicional.nuDsb.326.1';
+var RESOURCE_ID_DASH_ALVO = 'br.com.sankhya.menu.adicional.nuDsb.325.1';
